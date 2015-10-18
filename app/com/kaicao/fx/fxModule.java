cfe805f99dbb9d26a2c167aeb6b000a01b6ec0f5
kaicao <ck870711@hotmail.com>
2015-10-18 14:52:39 +0200

    Merge

--- app/com/kaicao/fx/fxModule.java
+++ app/com/kaicao/fx/fxModule.java
@@ -1,7 +1,18 @@
 package com.kaicao.fx;
 
+import com.google.inject.AbstractModule;
+import com.kaicao.fx.repository.FXTickDataRepository;
+import com.kaicao.fx.repository.impl.FXTickDataRepositoryImpl;
+import com.kaicao.fx.service.FXTickDataService;
+
 /**
  * Created by kaicao on 30/09/15.
  */
-public class fxModule {
+public class FXModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(FXTickDataRepository.class).to(FXTickDataRepositoryImpl.class);
+        bind(FXTickDataService.class);
+    }
 }
